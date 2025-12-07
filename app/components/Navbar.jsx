0f184ade6f6a0ca0b3bc6b5a32e7d11628f58fb0
erlis <erlisbunjaku54@gmail.com>
2025-12-07 21:08:40 +0100

    Remove My Skills

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -58,9 +58,6 @@ const Navbar = () => {
           <li>
             <a href="#services">Services</a>
           </li>
-             <li>
-            <a href="#services">My skills</a>
-          </li>
           <li>
             <a href="#work">My Work</a>
           </li>
